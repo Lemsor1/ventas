b7d7a6dd6a2b030888b40f2decd4915b0c421a74
Ross <Ross@DESKTOP-8VOCPSJ>
2019-06-16 22:33:25 -0600

    Cambiando frase motivadora

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola mundo.");
+            MessageBox.Show("Lo que te propongas en la vida lo puedes lograr!");
         }
     }
 }
